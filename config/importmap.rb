b3019b8bc7ac1010ff32d2efe6b3710d9e04b7e5
aejohnmartin <132005590+aejohnmartin@users.noreply.github.com>
2025-08-10 12:52:40 -0400

    Install DaisyUI for Tailwind components
    Install Tailwind Forms plugin.

--- config/importmap.rb
+++ config/importmap.rb
@@ -16,3 +16,6 @@ pin "postcss-selector-parser" # @6.0.10
 pin "tailwindcss/colors", to: "tailwindcss--colors.js" # @4.1.11
 pin "tailwindcss/plugin", to: "tailwindcss--plugin.js" # @4.1.11
 pin "util-deprecate" # @1.0.2
+pin "@tailwindcss/forms", to: "@tailwindcss--forms.js" # @0.5.10
+pin "mini-svg-data-uri" # @1.4.4
+pin "tailwindcss/defaultTheme", to: "tailwindcss--defaultTheme.js" # @4.1.11
