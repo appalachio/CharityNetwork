4e98c370b987bf4c8f47f02da249176f9949bd10
aejohnmartin <132005590+aejohnmartin@users.noreply.github.com>
2025-10-07 20:17:33 -0400

    Fix typo in production env config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :postmark
   config.action_mailer.postmark_settings = {
-    api_token: Rails.application.secrets.postmark_api_token
+    api_token: Rails.application.credentials.postmark_api_token
   }
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
